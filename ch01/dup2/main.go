d6b25f513de2a26fa5bc572878d1901ea0ddead7
Jay Patel <jay3686@gmail.com>
2017-05-12 00:31:15 -0400

    added exercise 1.4

--- ch01/dup2/main.go
+++ ch01/dup2/main.go
@@ -11,9 +11,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	countNames := make(map[string]map[string]struct{})
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, "", countNames, counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -21,21 +22,29 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, arg, countNames, counts)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s", n, line)
+			for name := range countNames[line] {
+				fmt.Printf("\t%s", name)
+			}
+			fmt.Println()
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, fileName string, countNames map[string]map[string]struct{}, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		if countNames[input.Text()] == nil {
+			countNames[input.Text()] = make(map[string]struct{})
+		}
+		countNames[input.Text()][fileName] = struct{}{}
 	}
 	// NOTE: ignoring errors for now bc lazy
 }
