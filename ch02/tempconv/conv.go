d8d9d369aee212f592f81b984f78004ff30dc87c
Jay Patel <jay3686@gmail.com>
2017-05-24 21:00:32 -0400

    added tempconv

---
+++ ch02/tempconv/conv.go
@@ -0,0 +1,5 @@
+// Package tempconv performs Celsius and Fahrenheit conversions.
+package tempconv
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
