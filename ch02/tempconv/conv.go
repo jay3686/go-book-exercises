09437190f9f79139ab5f7dbf19b06481785262c2
Jay Patel <jay3686@gmail.com>
2017-05-25 23:01:33 -0400

    ex 2.1

--- ch02/tempconv/conv.go
+++ ch02/tempconv/conv.go
@@ -2,4 +2,7 @@
 package tempconv
 
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func CtoK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
+func KtoC(c Kelvin) Celsius { return Celsius(c + 273.15) }
